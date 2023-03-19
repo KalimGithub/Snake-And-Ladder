be8e76043478ad8ede9a6be03389b39478b2e08a
KalimGithub <117559118+KalimGithub@users.noreply.github.com>
2023-03-19 22:40:33 +0530

    Delete java swing/gitProject directory

--- java swing/gitProject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.snakeandladder {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.snakeandladder to javafx.fxml;
-    exports com.example.snakeandladder;
-}
